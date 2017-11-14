4ae88d30757805d2f39afe1936d67810db615383
willdonner <765019300@qq.com>
2017-11-14 15:48:04 +0800

    add VGchartz_spider

---
+++ VGchartz_spider.py
@@ -0,0 +1,68 @@
+from bs4 import BeautifulSoup
+import urllib
+import pandas as pd 
+
+pages = 2
+rec_count =0
+rank = []
+game = []
+platform =[]
+year = []
+genre = []
+publisher = []
+sale_na = []
+sale_eu = []
+sale_jp = []
+sale_ot = []
+sale_gl = []
+
+urlhead = 'http://www.vgchartz.com/gamedb/'
+urltail = '&results=1000&name=&platform=&minSales=0&publisher=&genre=&sort=GL'
+
+for page in range(1,pages):
+    real_url = urlhead + str(page) +urltail
+    r = urllib.request.urlopen(real_url)
+    soup = BeautifulSoup(r,"lxml")
+    print("page:"+str(page))
+    chart = soup.find("table", class_="chart")
+    for row in chart.find_all('tr')[1:]:
+            try:
+                col = row.find_all('td')
+
+                column1 = col[0].string.strip()
+                column2 = col[1].string.strip()
+                column3 = col[2].string.strip()
+                column4 = col[3].string.strip()
+                column5 = col[4].string.strip()
+                column6 = col[5].string.strip()
+                column7 = col[6].string.strip()
+                column8 = col[7].string.strip()
+                column9 = col[8].string.strip()
+                column10 = col[9].string.strip()
+                column11 = col[10].string.strip()
+
+                #添加数据到list中
+
+                rank.append(column1)
+                game.append(column2)
+                platform.append(column3)
+                year.append(column4)
+                genre.append(column5)
+                publisher.append(column6)
+                sale_na.append(column7)
+                sale_eu.append(column8)
+                sale_jp.append(column9)
+                sale_row.append(column10)
+                sale_glob.append(column11)
+
+                rec_count += 1
+
+            except:
+                continue
+
+columns = {'rank': rank, 'name': game, 'platform': platform, 'year': year, 'genre': genre, 'publisher': publisher, 'NA_Sales':sales_na, 'EU_Sales': sales_eu,'JP_Sales': sales_jp,'Other_Sales':sales_ot, 'Global_Sales':sales_gl }
+print("rec_count"+str(rec_count))
+df = pd.DataFrame(columns)
+df = df[['Rank','Name','Platform','Year','Genre','Publisher','NA_Sales','EU_Sales','JP_Sales','Other_Sales','Global_Sales']]
+del df.index.name
+df.to_csv("vgsales.csv",sep=",",encoding='utf-8')
